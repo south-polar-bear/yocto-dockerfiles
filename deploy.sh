f46a7e48147b693db94dddd44d27b1c2399dffd2
wangbing1087 <97269848+wangbing1087@users.noreply.github.com>
2026-02-17 01:20:23 +0800

    Disable deployment for pull requests
    Comment out the deployment condition for pull requests.

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ fi
 
 # Don't deploy on pull requests because it could just be junk code that won't
 # get merged
-if ([ "${GITHUB_EVENT_NAME}" = "push" ] || [ "${GITHUB_EVENT_NAME}" = "workflow_dispatch" ] || [ "${GITHUB_EVENT_NAME}"  = "schedule" ])  && [ "${GITHUB_REF}" = "refs/heads/master" ]; then
+#if ([ "${GITHUB_EVENT_NAME}" = "push" ] || [ "${GITHUB_EVENT_NAME}" = "workflow_dispatch" ] || [ "${GITHUB_EVENT_NAME}"  = "schedule" ])  && [ "${GITHUB_REF}" = "refs/heads/master" ]; then
 #    echo $DOCKER_PASSWORD | ${ENGINE_CMD} login -u $DOCKER_USERNAME --password-stdin
 #    ${ENGINE_CMD} push $REPO:$DISTRO_TO_BUILD-base
 #    ${ENGINE_CMD} push $REPO:$DISTRO_TO_BUILD-builder
@@ -26,4 +26,4 @@ if ([ "${GITHUB_EVENT_NAME}" = "push" ] || [ "${GITHUB_EVENT_NAME}" = "workflow_
     echo $GHCR_PASSWORD | ${ENGINE_CMD} login ghcr.io -u $GHCR_USERNAME --password-stdin
     ${ENGINE_CMD} push ghcr.io/$REPO:$DISTRO_TO_BUILD-base
     ${ENGINE_CMD} push ghcr.io/$REPO:$DISTRO_TO_BUILD-builder
-fi
+#fi
